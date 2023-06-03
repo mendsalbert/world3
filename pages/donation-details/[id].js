38b86f0e77284e9844047d72358064512c03ddd3
mendsalbert <mendsalbert@gmail.com>
2023-06-03 22:17:30 +0000

    world3

--- pages/donation-details/[id].js
+++ pages/donation-details/[id].js
@@ -25,7 +25,7 @@ const DonationDetails = () => {
           return d.donationId.toString() == id;
         });
         console.log("f_res", f_res);
-        setDonation(f_res);
+        setDonation(f_res[0]);
       } catch (error) {
         console.log(error);
       }
